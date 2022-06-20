b8d5e7de53dbd70a1d6339c603aa644bccf0ce30
Emirton Sousa <emirtonf@gmail.com>
2022-06-20 16:02:34 -0300

    feat: function to navigate between page contents created

---
+++ app.js
@@ -0,0 +1,33 @@
+var navHome = document.getElementById("nav-home");
+var navCommunity = document.getElementById("nav-community");
+var navAboutus = document.getElementById("nav-aboutus");
+
+navHome.addEventListener('click', showHome);
+navCommunity.addEventListener('click', showCommunity);
+navAboutus.addEventListener('click', showAboutus);
+
+home = document.getElementById("home-container");
+community = document.getElementById("community-container");
+aboutus = document.getElementById("aboutus-container");
+    /**/
+
+function showHome() {
+    console.log('home');
+    home.style.display = 'flex';
+    community.style.display = 'none';
+    aboutus.style.display = 'none';
+}
+
+function showCommunity() {
+    console.log('community');
+    home.style.display = 'none';
+    community.style.display = 'flex';
+    aboutus.style.display = 'none';
+}
+
+function showAboutus() {
+    console.log('about us');
+    home.style.display = 'none';
+    community.style.display = 'none';
+    aboutus.style.display = 'block';
+}
