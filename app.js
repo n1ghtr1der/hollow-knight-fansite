9589948e7da8fdb66b592e011d2ce7ac58240c37
Emirton Sousa <emirtonf@gmail.com>
2022-06-23 14:05:37 -0300

    accessibility bar function added[].

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ home = document.getElementById("home-container");
 community = document.getElementById("community-container");
 aboutUs = document.getElementById("aboutus-container");
 
+
 function navigate(page){
     console.log(page);
     switch(page) {
@@ -9,33 +10,57 @@ function navigate(page){
             home.style.display = 'flex';
             community.style.display = 'none';
             aboutUs.style.display = 'none';
-            document.getElementById("nav-home").style.fontSize = '40px';
-            document.getElementById("nav-community").style.fontSize = '30px';
-            document.getElementById("nav-aboutus").style.fontSize = '30px';
             break;
         case "community":
             home.style.display = 'none';
             community.style.display = 'flex';
             aboutUs.style.display = 'none';
-            document.getElementById("nav-home").style.fontSize = '30px';
-            document.getElementById("nav-community").style.fontSize = '40px';
-            document.getElementById("nav-aboutus").style.fontSize = '30px';
             break;
         case "about-us":
             home.style.display = 'none';
             community.style.display = 'none';
             aboutUs.style.display = 'block';
-            document.getElementById("nav-home").style.fontSize = '30px';
-            document.getElementById("nav-community").style.fontSize = '30px';
-            document.getElementById("nav-aboutus").style.fontSize = '40px';
+            break;
+    }
+}
+
+    let element = document.getElementById("html");
+    let fontSize = getComputedStyle(element, null).getPropertyValue("font-size");
+    fontSize = parseInt(fontSize);
+
+    localStorage.setItem('globalFontSize', fontSize);
+
+
+function changeFontSize(type) {
+    let element = document.getElementById("html");
+    let fontSize = getComputedStyle(element, null).getPropertyValue("font-size");
+    fontSize = parseInt(fontSize);
+
+    switch(true) {
+        case type == 'increase-font' && fontSize < 24:
+            element.style.fontSize = (fontSize + 2) + "px";
+            break;
+        case type == 'decrease-font' && fontSize > 8:
+            element.style.fontSize = (fontSize - 2) + "px";
+            break;
+        case type == 'default-font':
+            fontSize = localStorage.getItem('globalFontSize');
+            element.style.fontSize = fontSize + "px";
             break;
     }
 }
 
 
-function changeFontSize(type) {
-    let elements = ["#nav-community", "#nav-home", "#nav-aboutus", "#home-title", "#home-description", "#community-title", "#community-description", "#about-us-title", "#about-us-description", "#lead-title", "#lead-description", "#mentoring-name1", "#mentoring-description1", "#mentoring-name2", "#mentoring-description2", "#mentoring-name3", "#mentoring-description3"];
-    
+
+
+
+
+
+//let elements = ["#nav-community", "#nav-home", "#nav-aboutus", "#home-title", "#home-description", "#community-title", "#community-description", "#about-us-title", "#about-us-description", "#lead-title", "#lead-description", "#mentoring-name1", "#mentoring-description1", "#mentoring-name2", "#mentoring-description2", "#mentoring-name3", "#mentoring-description3", "#steam-button", "#discord-button", "#teamcherry-button", "#lead-button"];
+
+
+/*function changeFontSize(type) {
+      
     breakElement = document.getElementById("home-title");
     breakpoint = getComputedStyle(breakElement, null).getPropertyValue("font-size");
     breakpoint = parseInt(breakpoint);
@@ -49,8 +74,8 @@ function changeFontSize(type) {
 
         if(type == 'increase-font' && breakpoint < 66){
             el.style.fontSize = (fontSize + 2) + "px";
-        }if (type == 'decrease-font' && breakpoint > 50) {
+        }if(type == 'decrease-font' && breakpoint > 50){
             el.style.fontSize = (fontSize - 2) + "px";
         }
     });
-}
+}*/
